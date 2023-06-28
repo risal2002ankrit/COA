078070a31c743cb8ae1a8497885cefd34a0decc8
Ankrit <risalankrit2002@gmail.com>
2023-06-28 07:50:19 +0545

    bug fixed

--- Full Adder/Adder.py
+++ Full Adder/Adder.py
@@ -1,51 +1,55 @@
 def decimaltobinary(dec_num, array):
-    i=7
+    i = 7
     while dec_num != 0:
         rem = dec_num % 2
-        array[i]=rem
-        dec_num= dec_num//2
-        i-=1
+        array[i] = rem
+        dec_num = dec_num//2
+        i -= 1
+
 
 def binarytodecimal(array):
-    deci=0,
-    i=0
-    for i in range(7,0):
-        deci= deci+ array[i] * pow(2,i)
-        i -= 1
+    deci = 0
+    j=7
+    for i in range(7):
+        deci = deci + array[j] * pow(2, i)
+        j -= 1
     return deci
 
+
 def showbinary(array):
- for i in array:
-     print(array, end= "")
- print("\n")
-def addbinary(number1,number2,result):
+    for i in array:
+        print(i, end="")
+    print("\n")
+
+
+def addbinary(number1, number2, result):
     c = 0
-    for i in range (7, -1, -1):
+    for i in range(7, -1, -1):
 
         result[i] = (number1[i] ^ number2[i] ^ c)
         c = ((number1[i] & number2[i]) | (number1[i] & c) | (number2[i] & c))
 
-        result[i] = c
-        return c
-def main(self):
-    numb1= [0,0,0,0,0,0,0,0,0,0]
-    numb2= [0,0,0,0,0,0,0,0,0,0]
-    result=[0,0,0,0,0,0,0,0,0,0]
-    dec=int(input("enter the decimal: "))
-    decimaltobinary(dec,numb1)
-    dec2=int(input("enter the decimal: "))
-    decimaltobinary(dec2,numb2)
+    result[i] = c
+    return c
+
+
+def main():
+    numb1 = [0, 0, 0, 0, 0, 0, 0, 0]
+    numb2 = [0, 0, 0, 0, 0, 0, 0, 0]
+    result = [0, 0, 0, 0, 0, 0, 0, 0]
+    dec = int(input("enter the decimal: "))
+    decimaltobinary(dec, numb1)
+    dec2 = int(input("enter the decimal: "))
+    decimaltobinary(dec2, numb2)
     showbinary(numb1)
     showbinary(numb2)
-    add = addbinary(numb1,numb2,result)
-    decimal= binarytodecimal(result)
-    if(add==0):
+    add = addbinary(numb1, numb2, result)
+    decimal = binarytodecimal(result)
+    if add == 0:
         print("the eqivalent result is")
         showbinary(result)
+        print("equivalent decimal")
+        print(decimal)
     else:
         print("overload")
-
-
-
-
-
+main()
